53df3b5318897e7ff476f6dcda9c7c2cf5adae88
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-08-03 00:11:35 -0500

    try influxdb

--- arduino_temperature.py
+++ arduino_temperature.py
@@ -10,6 +10,35 @@ import json
 
 # apt-get install -y python3-serial
 
+# apt-get install -y influxdb
+# apt-get install -y python3-influxdb
+
+
+
+# from influxdb import InfluxDBClient
+
+#json_body = [
+#    {
+#        "measurement": "temperature",
+#        "time": "2009-11-10T23:00:00Z",
+#        "fields": {
+#            "value": 0.64
+#        }
+#    }
+#]
+
+
+client = InfluxDBClient('localhost', 8086, 'root', 'root', 'sensor')
+
+client.create_database('sensor')
+
+#client.write_points(json_body)
+
+
+#result = client.query('select value from temperature;')
+
+print("Result: {0}".format(result))
+
 #line = '17/Dec/2011:09:48:49 -0600'
 #line = line.split(' ')[0]
 #print  time.strptime(line,"%d/%b/%Y:%H:%M:%S")
@@ -31,6 +60,15 @@ with Serial(device, 9600, timeout=8, writeTimeout=8) as serial:
                     
                     data['timestamp'] = datetime.now().isoformat()
                     print(json.dumps(data))
+                    client.write_points([
+                        {
+                            "measurement": "temperature",
+                            "time": data['timestamp'],
+                            "fields": {
+                                "value": data['value']
+                            }
+                        }
+                    ])
                 else:
                     print("error parsing, got: \"%s\"" % (line))
             else:
@@ -38,7 +76,7 @@ with Serial(device, 9600, timeout=8, writeTimeout=8) as serial:
         except Exception as e:
             print(str(e))
             break
-        time.sleep(30)
+        time.sleep(5)
         
         
         
